cd5da8c11fdbb41d54805165b10e5d8b9c81623e
HP <anusha.udupa@gmail.com>
2019-09-28 12:05:01 +0530

    trytkinter

--- testing.py
+++ testing.py
@@ -1,1 +1,16 @@
-print("hey")
+import os
+from tkinter import *
+os.system('clear')
+root=Tk()
+root.title("wow tkinter")
+root.geometry('400x600')
+def do():
+	dolabel = Label(root,text="hello "+ myText. get())
+	dolabel.pack()
+mylabel = Label(root , text="ur name please")
+mylabel.pack()
+myText = Entry(root, width="30")
+myText.pack()
+myButton = Button(root, text="commit",command=do)
+myButton.pack()
+root.mainloop()
